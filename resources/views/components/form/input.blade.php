3bf2603c3212291c085889c00a047ee02e3af219
BablooKumarWebzent <babloo.kumar@webzent.in>
2026-01-27 11:40:18 +0530

    ILL-93 UI Fixes

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,39 +1,4 @@
-{{-- @props([
-    'model' => '',
-    'name' => '',
-    'placeholder' => '',
-    'type' => 'text',
-    'filter' => null
-])
 
-<div class="space-y-1">
-    <input
-        type="{{ $type }}"
-        name="{{ $name }}"
-        placeholder="{{ $placeholder }}"
-        value="{{ old($name) }}"
-        class="input-base
-            @error($name) border-red-500 @enderror
-        "
-        x-model="form.{{ $model }}"
-        @input="
-            sanitizeField('{{ $model }}', '{{ $filter }}');
-            validateField('{{ $model }}');
-        "
-        :class="{
-            'border-red-500': errors.{{ $model }},
-            'border-green-500': !errors.{{ $model }} && form.{{ $model }}
-        }"
-    > --}}
-
-    {{-- Alpine validation error --}}
-    {{-- <p class="text-red-500 text-sm" x-text="errors.{{ $model }}"></p> --}}
-
-    {{-- Laravel validation error --}}
-    {{-- @error($name)
-        <p class="text-red-500 text-sm">{{ $message }}</p>
-    @enderror
-</div> --}}
 @props([
     'name',
     'placeholder' => '',
