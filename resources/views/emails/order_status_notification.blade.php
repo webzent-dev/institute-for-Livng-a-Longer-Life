7437206c51f18c8f441aa09323dcd2c6313550ac
Dinesh Kumar <dinesh.kumar@webzent.in>
2026-07-11 14:30:37 +0530

    fixed tracking number in mail template if ordr status is shipped

--- resources/views/emails/order_status_notification.blade.php
+++ resources/views/emails/order_status_notification.blade.php
@@ -4,8 +4,18 @@
         Dear {{ $order->first_name }},
     </p>
     <p style="font-size:16px;color:#555;">
-        We are excited to inform you that your order with ID <strong>{{ $order->order_number }}</strong> has been updated to the status: <strong>{{ $order->status }}</strong>.
+        We are excited to inform you that your order with ID <strong>{{ $order->order_number }}</strong> has been
+        updated to the status: <strong>{{ $order->status }}</strong>.
     </p>
+
+    @if ($subOrder)
+        @if ($subOrder->tracking_number)
+            <p style="font-size:16px;color:#555;">
+                <strong>Tracking number </strong>: <strong>{{ $subOrder->tracking_number }}</strong>
+            </p>
+        @endif
+    @endif
+
     <p style="font-size:16px;color:#555;">
         Best regards,<br>
         Institute for Living a Longer Life Team
