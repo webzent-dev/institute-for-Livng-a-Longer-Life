4d3e407709f9f27ac6e2f6c2779bd5424cbbd14f
Dinesh Kumar <dinesh.kumar@webzent.in>
2026-07-20 17:21:19 +0530

    admin panel munues icons are updated

--- resources/views/components/dashboard/sidebar/menu.blade.php
+++ resources/views/components/dashboard/sidebar/menu.blade.php
@@ -5,25 +5,25 @@
 <x-dashboard.sidebar.nav_link icon="layout-dashboard" label="Dashboard" route="{{ $role === 'collaborator' ? 'collaborator.dashboard' : 'admin.dashboard' }}"/>
 @if(in_array($role, ['admin','super_admin']))
     <x-dashboard.sidebar.nav_link icon="users" label="Users" route="admin.users.index" />
-    <x-dashboard.sidebar.nav_link icon="user" label="Collaborators" route="collaborators.index" />
+    <x-dashboard.sidebar.nav_link icon="handshake" label="Collaborators" route="collaborators.index" />
     <x-dashboard.sidebar.nav_link icon="package-search" label="Products" route="admin.products" />
     <x-dashboard.sidebar.nav_link icon="shopping-cart" label="Orders" route="admin.orders" />
     <x-dashboard.sidebar.nav_link icon="graduation-cap" label="Courses"  route="admin.courses" />
-    <x-dashboard.sidebar.nav_link icon="user-plus" label="Memberships Plans"  route="admin.manage-membership" />
+    <x-dashboard.sidebar.nav_link icon="crown" label="Memberships Plans"  route="admin.manage-membership" />
     <x-dashboard.sidebar.nav_link icon="briefcase" label="Admin Business Details" route="admin.business-details" />
     <x-dashboard.sidebar.nav_link icon="settings" label="Settings" route="admin.settings.index"  />
-    <x-dashboard.sidebar.nav_link icon="dollar-sign" label="Zoom Sessions" route="admin.zoom-sessions" />
-    <x-dashboard.sidebar.nav_link icon="globe" label="Content Management" route="admin.content.management" />
+    <x-dashboard.sidebar.nav_link icon="webcam" label="Zoom Sessions" route="admin.zoom-sessions" />
+    <x-dashboard.sidebar.nav_link icon="layout-template" label="Content Management" route="admin.content.management" />
     <x-dashboard.sidebar.nav_link icon="map-pin" label="Locations" route="admin.locations" />
-    <x-dashboard.sidebar.nav_link icon="star" label="Testimonials" route="admin.testimonials" />
-    <x-dashboard.sidebar.nav_link icon="star" label="Video Testimonials" route="admin.video-testimonials" />
-    <x-dashboard.sidebar.nav_link icon="star" label="FAQ Categories" route="admin.faq-categories" />
-    <x-dashboard.sidebar.nav_link icon="star" label="FAQs" route="admin.faqs" />
-    <x-dashboard.sidebar.nav_link icon="star" label="Help Categories" route="admin.help-categories" />
-    <x-dashboard.sidebar.nav_link icon="star" label="Help Articles" route="admin.help-articles" />
-    <x-dashboard.sidebar.nav_link icon="star" label="Intro Videos" route="admin.intro-videos" />
+    <x-dashboard.sidebar.nav_link icon="quote" label="Testimonials" route="admin.testimonials" />
+    <x-dashboard.sidebar.nav_link icon="video" label="Video Testimonials" route="admin.video-testimonials" />
+    <x-dashboard.sidebar.nav_link icon="folder-tree" label="FAQ Categories" route="admin.faq-categories" />
+    <x-dashboard.sidebar.nav_link icon="messages-square" label="FAQs" route="admin.faqs" />
+    <x-dashboard.sidebar.nav_link icon="library" label="Help Categories" route="admin.help-categories" />
+    <x-dashboard.sidebar.nav_link icon="book-open" label="Help Articles" route="admin.help-articles" />
+    <x-dashboard.sidebar.nav_link icon="circle-play" label="Intro Videos" route="admin.intro-videos" />
     <x-dashboard.sidebar.nav_link icon="mail" label="Email Management" route="admin.email.index" />
-    <x-dashboard.sidebar.nav_link icon="user" label="Subscribers" route="admin.subscribers" />
+    <x-dashboard.sidebar.nav_link icon="bell" label="Subscribers" route="admin.subscribers" />
     <!-- <x-dashboard.sidebar.nav_link icon="file-text" label="Audit Logs" route="admin.audit.logs" /> -->
 @endif
 
